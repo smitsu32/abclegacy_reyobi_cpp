d41bc457e55519446670deb76f1b1a179a3cac01
smitsu32 <33113176@stn.nitech.ac.jp>
2026-02-27 04:37:23 +0900

    Refactor code structure for improved readability and maintainability

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<(n);i++)
+#define reps(i,s,n) for(int i=(s);i<(s)+(int)(n);i++)
+
+int main() {
+    cout<<"Hello World"<<endl;
+}
